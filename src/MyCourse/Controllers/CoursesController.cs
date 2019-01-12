dfc5678b5dd07f0471a465fc637496d4ba2fb164
Moreno Gentili <moreno@smartmobile.it>
2019-01-12 19:39:51 +0100

    Aggiunte view di contenuto e una view di layout

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -6,12 +6,12 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Sono Index");
+            return View();
         }
 
         public IActionResult Detail(string id)
         {
-            return Content($"Sono Detail, ho ricevuto l'id {id}");
+            return View();
         }
     }
 }
--- src/MyCourse/Controllers/HomeController.cs
+++ src/MyCourse/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return Content("Sono la index della home");
+            return View();
         }
     }
 }
