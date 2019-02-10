2b223e7a6c174dca991b2d478d7588ae361dfcdb
Moreno Gentili <moreno@smartmobile.it>
2019-02-10 20:58:49 +0100

    Registrato il servizio CourseService

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -7,17 +7,20 @@ namespace MyCourse.Controllers
 {
     public class CoursesController : Controller
     {
+        private readonly CourseService courseService;
+        public CoursesController(CourseService courseService)
+        {
+            this.courseService = courseService;
+        }
         public IActionResult Index()
         {
             ViewData["Title"] = "Catalogo dei corsi";
-            var courseService = new CourseService();
             List<CourseViewModel> courses = courseService.GetCourses();
             return View(courses);
         }
 
         public IActionResult Detail(int id)
         {
-            var courseService = new CourseService();
             CourseDetailViewModel viewModel = courseService.GetCourse(id);
             ViewData["Title"] = viewModel.Title;
             return View(viewModel);
--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using MyCourse.Models.Services.Application;
 
 namespace MyCourse
 {
@@ -18,6 +19,7 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddTransient<CourseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
