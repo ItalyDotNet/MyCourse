af1d0468048635e69ce1fb165ea5c130a9949ddd
Moreno Gentili <moreno@smartmobile.it>
2019-04-22 21:52:04 +0200

    Usati i metodi di mapping

--- src/MyCourse/Models/Services/Application/EfCoreCourseService.cs
+++ src/MyCourse/Models/Services/Application/EfCoreCourseService.cs
@@ -19,30 +19,14 @@ namespace MyCourse.Models.Services.Application
         public async Task<CourseDetailViewModel> GetCourseAsync(int id)
         {
             IQueryable<CourseDetailViewModel> queryLinq = dbContext.Courses
+                .Include(course => course.Lessons)
                 .Where(course => course.Id == id)
-                .Select(course => new CourseDetailViewModel
-                {
-                    Id = course.Id,
-                    Title = course.Title,
-                    Description = course.Description,
-                    Author = course.Author,
-                    ImagePath = course.ImagePath,
-                    Rating = course.Rating,
-                    CurrentPrice = course.CurrentPrice,
-                    FullPrice = course.FullPrice,
-                    Lessons = course.Lessons.Select(lesson => new LessonViewModel
-                    {
-                        Id = lesson.Id,
-                        Title = lesson.Title,
-                        Description = lesson.Description,
-                        Duration = lesson.Duration
-                    }).ToList()
-                });
+                .Select(course => CourseDetailViewModel.FromEntity(course)); //Usando metodi statici come FromEntity, la query potrebbe essere inefficiente. Mantenere il mapping nella lambda oppure usare un extension method personalizzato
             
-            //.FirstOrDefaultAsync(); //Restituisce null se l'elenco è vuoto e non solleva mai un'eccezione
-            //.SingleOrDefaultAsync(); //Tollera il fatto che l'elenco sia vuoto e in quel caso restituisce null, oppure se l'elenco contiene più di 1 elemento, solleva un'eccezione
-            //.FirstAsync(); //Restituisce il primo elemento, ma se l'elenco è vuoto solleva un'eccezione
             CourseDetailViewModel viewModel = await queryLinq.SingleAsync();
+                                                           //.FirstOrDefaultAsync(); //Restituisce null se l'elenco è vuoto e non solleva mai un'eccezione
+                                                           //.SingleOrDefaultAsync(); //Tollera il fatto che l'elenco sia vuoto e in quel caso restituisce null, oppure se l'elenco contiene più di 1 elemento, solleva un'eccezione
+                                                           //.FirstAsync(); //Restituisce il primo elemento, ma se l'elenco è vuoto solleva un'eccezione
                 
             return viewModel;
         }
@@ -50,16 +34,7 @@ namespace MyCourse.Models.Services.Application
         public async Task<List<CourseViewModel>> GetCoursesAsync()
         {
             IQueryable<CourseViewModel> queryLinq = dbContext.Courses
-                .Select(course => 
-                    new CourseViewModel {
-                        Id = course.Id,
-                        Title = course.Title,
-                        ImagePath = course.ImagePath,
-                        Author = course.Author,
-                        Rating = course.Rating,
-                        CurrentPrice = course.CurrentPrice,
-                        FullPrice = course.FullPrice
-                });
+                .Select(course => CourseViewModel.FromEntity(course)); //Usando metodi statici come FromEntity, la query potrebbe essere inefficiente. Mantenere il mapping nella lambda oppure usare un extension method personalizzato
 
             List<CourseViewModel> courses = await queryLinq.ToListAsync(); //La query al database viene inviata qui, quando manifestiamo l'intenzione di voler leggere i risultati
 
