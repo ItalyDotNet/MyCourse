ac1260425709f0baef9e88215aead523619ab764
Moreno Gentili <moreno@smartmobile.it>
2019-05-12 19:10:36 +0200

    Rimosso codice inutile

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -48,10 +48,5 @@ namespace MyCourse
                 routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
-
-        private void UpdateReloadFileForBrowserSync(IHostingEnvironment env, IApplicationLifetime lifetime)
-        {
-            
-        }
     }
 }
