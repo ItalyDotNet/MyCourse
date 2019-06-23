d5356786e838266e547cac7111733fbec499c7a9
Moreno Gentili <moreno@smartmobile.it>
2019-06-23 23:31:22 +0200

    Aggiunti i pacchetti NuGet per Serilog

--- src/MyCourse/Program.cs
+++ src/MyCourse/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace MyCourse
 {
@@ -20,20 +13,21 @@ namespace MyCourse
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 //Posso ridefinire l'elenco dei provider di default
-                /*.ConfigureLogging((context, builder) => {
+                /*.ConfigureLogging((context, builder) => 
+                {
                     builder.ClearProviders();
                     builder.AddConsole();
                     builder.Add...;
                 })*/
 
                 //Posso ridefinire l'elenco delle fonti di configurazione con ConfigureAppConfiguration
-                /*.ConfigureAppConfiguration((context, builder) => {
+                /*.ConfigureAppConfiguration((context, builder) => 
+                {
                     builder.Sources.Clear();
                     builder.AddJsonFile("appsettings.json", optional:true, reloadOnChange: true);
                     builder.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
                     //Qui altre fonti...
                 })*/
-
                 .UseStartup<Startup>();
     }
 }
