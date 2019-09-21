63db8fa32e4bfbfe55997d3678fefefa254edaa1
Moreno Gentili <moreno@smartmobile.it>
2019-09-21 22:48:22 +0200

    Il ViewComponent di paginazione è ora riutilizzabile

--- src/MyCourse/Models/ViewModels/CourseListViewModel.cs
+++ src/MyCourse/Models/ViewModels/CourseListViewModel.cs
@@ -3,9 +3,26 @@ using MyCourse.Models.InputModels;
 
 namespace MyCourse.Models.ViewModels
 {
-    public class CourseListViewModel
+    public class CourseListViewModel : IPaginationInfo
     {
         public ListViewModel<CourseViewModel> Courses { get; set; }
         public CourseListInputModel Input { get; set; }
+
+
+
+
+        #region Implementazione IPaginationInfo
+        int IPaginationInfo.CurrentPage => Input.Page;
+
+        int IPaginationInfo.TotalResults => Courses.TotalCount;
+
+        int IPaginationInfo.ResultsPerPage => Input.Limit;
+
+        string IPaginationInfo.Search => Input.Search;
+
+        string IPaginationInfo.OrderBy => Input.OrderBy;
+
+        bool IPaginationInfo.Ascending => Input.Ascending;
+        #endregion
     }
 }
