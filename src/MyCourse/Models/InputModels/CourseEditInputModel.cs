fe8883caa2c395acc57967f2468f9e816e1080a8
Moreno Gentili <brightsoul@tiscali.it>
2020-01-21 23:35:43 +0100

    Aggiunto il supporto all'apice nei titoli del corso

--- src/MyCourse/Models/InputModels/CourseEditInputModel.cs
+++ src/MyCourse/Models/InputModels/CourseEditInputModel.cs
@@ -18,7 +18,7 @@ namespace MyCourse.Models.InputModels
         [Required(ErrorMessage = "Il titolo è obbligatorio"),
          MinLength(10, ErrorMessage = "Il titolo dev'essere di almeno {1} caratteri"),
          MaxLength(100, ErrorMessage = "Il titolo dev'essere di al massimo {1} caratteri"),
-         RegularExpression(@"^[\w\s\.]+$", ErrorMessage = "Titolo non valido"),
+         RegularExpression(@"^[\w\s\.']+$", ErrorMessage = "Titolo non valido"),
          Remote(action: nameof(CoursesController.IsTitleAvailable), controller: "Courses", ErrorMessage = "Il titolo esiste già", AdditionalFields = "Id"),
          Display(Name = "Titolo")]
         public string Title { get; set; }
