4d4c19ab2b6d27ae1528cff336963e628600ae60
Moreno Gentili <moreno@smartmobile.it>
2020-03-21 19:57:08 +0100

    Impostati limiti di dimensione e concorrenza nel salvataggio delle immagini

--- src/MyCourse/Models/Services/Infrastructure/MagickNetImagePersister.cs
+++ src/MyCourse/Models/Services/Infrastructure/MagickNetImagePersister.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using ImageMagick;
 using Microsoft.AspNetCore.Hosting;
@@ -10,35 +11,46 @@ namespace MyCourse.Models.Services.Infrastructure
     {
         private readonly IWebHostEnvironment env;
 
+        private readonly SemaphoreSlim semaphore;
+
         public MagickNetImagePersister(IWebHostEnvironment env)
         {
+            ResourceLimits.Height = 4000;
+            ResourceLimits.Width = 4000;
+            semaphore = new SemaphoreSlim(2);
             this.env = env;
         }
 
-        public Task<string> SaveCourseImageAsync(int courseId, IFormFile formFile)
+        public async Task<string> SaveCourseImageAsync(int courseId, IFormFile formFile)
         {
-            //Salvare il file
-            string path = $"/Courses/{courseId}.jpg";
-            string physicalPath = Path.Combine(env.WebRootPath, "Courses", $"{courseId}.jpg");
-
-            using Stream inputStream = formFile.OpenReadStream();
-            using MagickImage image = new MagickImage(inputStream);
-
-            //Manipolare l'immagine
-            int width = 300;  //Esercizio: ottenere questi valori dalla configurazione
-            int height = 300;
-            MagickGeometry resizeGeometry = new MagickGeometry(width, height)
+            await semaphore.WaitAsync();
+            try
             {
-                FillArea = true
-            };
-            image.Resize(resizeGeometry);
-            image.Crop(width, width, Gravity.Northwest);
+                //Salvare il file
+                string path = $"/Courses/{courseId}.jpg";
+                string physicalPath = Path.Combine(env.WebRootPath, "Courses", $"{courseId}.jpg");
 
-            image.Quality = 70;
-            image.Write(physicalPath, MagickFormat.Jpg);
+                using Stream inputStream = formFile.OpenReadStream();
+                using MagickImage image = new MagickImage(inputStream);
 
-            //Restituire il percorso al file
-            return Task.FromResult(path);
+                //Manipolare l'immagine
+                int width = 300;  //Esercizio: ottenere questi valori dalla configurazione
+                int height = 300;
+                MagickGeometry resizeGeometry = new MagickGeometry(width, height)
+                {
+                    FillArea = true
+                };
+                image.Resize(resizeGeometry);
+                image.Crop(width, width, Gravity.Northwest);
+
+                image.Quality = 70;
+                image.Write(physicalPath, MagickFormat.Jpg);
+
+                //Restituire il percorso al file
+                return path;
+            } finally {
+                semaphore.Release();
+            }
         }
     }
 }
