474e065897b44ba4a124982a94948e36e082be55
Moreno Gentili <brightsoul@tiscali.it>
2020-10-25 10:33:19 +0100

    Aggiunto il mapping tra DataRow e ApplicationUser

--- src/MyCourse/Models/Entities/ApplicationUser.cs
+++ src/MyCourse/Models/Entities/ApplicationUser.cs
@@ -10,9 +10,28 @@ namespace MyCourse.Models.Entities
         public string FullName { get; set; }
         public virtual ICollection<Course> AuthoredCourses { get; set; }
 
-        public static ApplicationUser FromDataRow(DataRow row)
+        public static ApplicationUser FromDataRow(DataRow courseRow)
         {
-            throw new NotImplementedException();
+            var applicationUser = new ApplicationUser
+            {
+                Id = Convert.ToString(courseRow["Id"]),
+                UserName = Convert.ToString(courseRow["UserName"]),
+                NormalizedUserName = Convert.ToString(courseRow["NormalizedUserName"]),
+                Email = Convert.ToString(courseRow["Email"]),
+                NormalizedEmail = Convert.ToString(courseRow["NormalizedEmail"]),
+                EmailConfirmed = Convert.ToBoolean(courseRow["EmailConfirmed"]),
+                PasswordHash = Convert.ToString(courseRow["PasswordHash"]),
+                SecurityStamp = Convert.ToString(courseRow["SecurityStamp"]),
+                ConcurrencyStamp = Convert.ToString(courseRow["ConcurrencyStamp"]),
+                PhoneNumber = Convert.ToString(courseRow["PhoneNumber"]),
+                PhoneNumberConfirmed = Convert.ToBoolean(courseRow["PhoneNumberConfirmed"]),
+                TwoFactorEnabled = Convert.ToBoolean(courseRow["TwoFactorEnabled"]),
+                LockoutEnd = (courseRow["LockoutEnd"] == DBNull.Value ? (DateTime?) null : Convert.ToDateTime(courseRow["LockoutEnd"])),
+                LockoutEnabled = Convert.ToBoolean(courseRow["LockoutEnabled"]),
+                AccessFailedCount = Convert.ToInt32(courseRow["AccessFailedCount"]),
+                FullName = Convert.ToString(courseRow["FullName"])
+            };
+            return applicationUser;
         }
     }
 }
