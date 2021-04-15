65a3cb9a630338e31a3ccc5e3a6ad4b3c90ed2b7
Moreno Gentili <brightsoul@tiscali.it>
2021-04-15 20:18:47 +0200

    Assegnazione e revoca dei ruoli

--- src/MyCourse/Pages/Admin/Users.cshtml.cs
+++ src/MyCourse/Pages/Admin/Users.cshtml.cs
@@ -1,15 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using MyCourse.Models.Entities;
+using MyCourse.Models.Enums;
 using MyCourse.Models.InputModels.Users;
 
 namespace MyCourse.Pages.Admin
 {
     public class UsersModel : PageModel
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UsersModel(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [BindProperty]
         public UserRoleInputModel Input { get; set; }
-        
+
         public IActionResult OnGet()
         {
             ViewData["Title"] = "Gestione utenti";
@@ -18,27 +33,70 @@ namespace MyCourse.Pages.Admin
 
         public async Task<IActionResult> OnPostAssignAsync()
         {
-            // Verifichiamo se ModelState.IsValid è true
-            // Con lo UserManager recuperiamo l'utente dal database in base all'email
-            // Con lo UserManager recuperiamo gli attuali claim dell'utente
-            // Verifichiamo se quell'utente ha già quel ruolo
-            // Se ce l'ha diamo un errore
-            // Se non ce l'ha aggiungiamo il claim del ruolo
-            // Diamo conferma all'utente e lo reindirizziamo
+            if (!ModelState.IsValid)
+            {
+                return OnGet();
+            }
+
+            ApplicationUser user = await userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError(nameof(Input.Email), $"L'indirizzo email {Input.Email} non corrisponde ad alcun utente");
+                return OnGet();
+            }
+
+            IList<Claim> claims = await userManager.GetClaimsAsync(user);
+
+            Claim roleClaim = new (ClaimTypes.Role, Input.Role.ToString());
+            if (claims.Any(claim => claim.Type == roleClaim.Type && claim.Value == roleClaim.Value))
+            {
+                ModelState.AddModelError(nameof(Input.Role), $"Il ruolo {Input.Role} è già assegnato all'utente {Input.Email}");
+                return OnGet();
+            }
+
+            IdentityResult result = await userManager.AddClaimAsync(user, roleClaim);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, $"L'operazione è fallita: {result.Errors.FirstOrDefault()?.Description}");
+                return OnGet();
+            }
+
+            TempData["ConfirmationMessage"] = $"Il ruolo {Input.Role} è stato assegnato all'utente {Input.Email}";
             return RedirectToPage();
         }
 
         public async Task<IActionResult> OnPostRevokeAsync()
         {
-            // Verifichiamo se ModelState.IsValid è true
-            // Con lo UserManager recuperiamo l'utente dal database in base all'email
-            // Con lo UserManager recuperiamo gli attuali claim dell'utente
-            // Verifichiamo se quell'utente ha già quel ruolo
-            // Se non ce l'ha diamo un errore
-            // Se ce l'ha rimuoviamo il claim del ruolo
-            // Diamo conferma all'utente e lo reindirizziamo
+            if (!ModelState.IsValid)
+            {
+                return OnGet();
+            }
+
+            ApplicationUser user = await userManager.FindByEmailAsync(Input.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError(nameof(Input.Email), $"L'indirizzo email {Input.Email} non corrisponde ad alcun utente");
+                return OnGet();
+            }
+
+            IList<Claim> claims = await userManager.GetClaimsAsync(user);
+
+            Claim roleClaim = new (ClaimTypes.Role, Input.Role.ToString());
+            if (!claims.Any(claim => claim.Type == roleClaim.Type && claim.Value == roleClaim.Value))
+            {
+                ModelState.AddModelError(nameof(Input.Role), $"Il ruolo {Input.Role} non era assegnato all'utente {Input.Email}");
+                return OnGet();
+            }
+
+            IdentityResult result = await userManager.RemoveClaimAsync(user, roleClaim);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, $"L'operazione è fallita: {result.Errors.FirstOrDefault()?.Description}");
+                return OnGet();
+            }
+
+            TempData["ConfirmationMessage"] = $"Il ruolo {Input.Role} è stato revocato all'utente {Input.Email}";
             return RedirectToPage();
         }
-
     }
 }
